a933efd016d66d824c29fcb6a4fdee5e0741c3bb
Jason <utdd@qq.com>
2023-05-18 16:05:20 +0800

    first

---
+++ src/main.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import Vue from 'vue/dist/vue.esm.js'
+import App from '@/App'
+import axios from 'axios'
+new Vue({
+  el:'#app',
+  components:{
+    App,
+  },
+  template:'<App />'
+})
+
+new Vue({
+  el:'#app',
+  render: h => h(App)
+})
